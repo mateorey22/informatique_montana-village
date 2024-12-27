2a173fe1a844716ed9df489f7a6c12ce9277b913
mateorey22 <149890580+mateorey22@users.noreply.github.com>
2024-12-27 10:56:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/cours-informatique/', // Doit correspondre au nom de votre repository
+  build: {
+    outDir: 'dist'
+  }
+})
